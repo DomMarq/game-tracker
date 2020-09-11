419d6f1168f6767acf11e726fe75b2282bb6f635
DomMarq <dominicmarques21@gmail.com>
2020-09-10 21:15:45 -0400

    added put service, material design

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ angular.module('app', ['ngMaterial', 'ngMessages']);
 const home = {
     templateUrl: './home/home.html',
     controller: 'HomeController'
-}
+};
 
 // Home Component with Routing (Routed / Stateful)
 angular.module('app').component('home', home);
@@ -19,44 +19,66 @@ angular.module('app').controller('HomeController', ['ExampleService', function (
 const settings = {
     templateUrl: '',
     controller: 'SettingsController'
-}
+};
 
 // Settings Component with Routing (Routed / Stateful)
-angular.module('app').component('settings', settings)
+angular.module('app').component('settings', settings);
 
 // Settings Controller with dependency injection using $inject method
 function SettingsController(ExampleService) {
 
-}
+};
 SettingsController.$inject = ['ExampleService'];
 angular.module('app').controller('SettingsController', SettingsController);
 /*--------------------- End Settings Component ---------------------*/
 
 /*--------------------- Example Service ---------------------*/
-function ExampleService() {
+function ExampleService($http) {
     // Services are Singletons
     // Properties
     // Methods
-}
-angular.module('app').service('ExampleService', ExampleService)
+    this.getData = getData;
+    this.postData = postData;
+    
+    // Get data from API
+    function getData() {
+        return $http({
+            method: 'GET',
+            url: ''
+        });
+    }
+    
+    // send data to backend
+    function postData(data) {
+        return $http({
+            method: 'PUT',
+            url: './data.json',
+            data: data,
+        })
+    }
+};
+ExampleService.$inject = ['$http'];
+angular.module('app').service('ExampleService', ExampleService);
 /*--------------------- End Example Service ---------------------*/
 
 /*--------------------- New Room Component ---------------------*/
 const newRoom = {
     templateUrl: './new-room/new-room.html',
     controller: 'NewRoomController'
-}
+};
 
 angular.module('app').component('newRoom', newRoom);
 
-function NewRoomController(ExampleService) {
-    this.createRoom = function(room) {
-        // TODO: transform info to JSON
+angular.module('app').controller('NewRoomController', ['ExampleService', function (ExampleService) {
+    this.createRoom = createRoom;
+    
+    function createRoom(room) {
+        console.log('Hello, world!');
+        console.log(angular.toJson(room));
+        ExampleService.postData(room);
         // TODO: send created JSON to backend through service
     };
     
-}
-NewRoomController.$inject = ['ExampleService'];
-angular.module('app').controller('SettingsController', SettingsController);
+}]);
 
 /*--------------------- End New Room Component ---------------------*/
