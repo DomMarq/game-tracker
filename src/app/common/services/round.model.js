4ba52832b0951fb5374e08cbca0f054ce302308d
Dominic Marques <dominicmarques21@gmail.com>
2020-09-29 16:05:02 -0400

    Round model starter

---
+++ src/app/common/services/round.model.js
@@ -0,0 +1,62 @@
+class RoundModel {
+    constructor(Parse) {
+        this.Parse = Parse;
+        this.name = "Round";
+        this.fields = [
+            'winner',
+            'teams',
+            'winnerScore',
+            'loserScore',
+            'loser',
+            'room',
+        ];
+        this.data = {};  // hold singular result of queries
+        this.collection = [];  // hold array results of queries
+    }
+
+    New(obj) {
+        // create a new Round Parse Object
+        if (angular.isUndefined(obj)) {
+            const parseObject = new this.Parse.Object(this.name);
+            this.Parse.defineAttributes(parseObject, this.fields);
+            return parseObject;
+        } else {  // Exposing Round Parse Object Attributes (getters and setters)
+            this.Parse.defineAttributes(obj, this.fields);
+            return obj;
+        }
+    }
+
+    getById(id) {
+        return new this.Parse.Query(this.New())
+            .include('teams')
+            .get(id)
+            .then(result => {
+                this.Parse.defineAttributes(result, this.fields);
+                // get the pointer
+                this.Parse.defineAttributes(result.teams, this.TeamModel.fields);
+                return Promise.resolve(result);
+            })
+            .catch(error => Promise.reject(error));
+    }
+
+    getByManager(manager) {
+        // get rounds by manager
+        return new this.Parse.Query(this.New())
+            .include('manager')
+            .equalTo('manager', manager)
+            .descending('createdAt')
+            .find()
+            .then(results => {
+                results.forEach(result => {
+                    this.Parse.defineAttributes(result, this.fields);
+                    this.Parse.defineAttributes(result.manager, this.UserModel.fields);
+                })
+                return Promise.resolve(results);
+            })
+            .catch(error => Promise.reject(error))
+    }
+
+    getByUser(user) {
+        return user;
+    }
+}
