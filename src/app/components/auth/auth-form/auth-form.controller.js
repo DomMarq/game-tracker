4a90fe668162e5dbe8e5a1375ce36002ca7b5939
Dominic Marques <dominicmarques21@gmail.com>
2020-10-11 23:28:35 -0400

    Initial attemps at getting the data binding to work

--- src/app/components/auth/auth-form/auth-form.controller.js
+++ src/app/components/auth/auth-form/auth-form.controller.js
@@ -1,9 +1,22 @@
-function AuthFormController () {
-  const $ctrl = this;
+function AuthFormController() {
+    const $ctrl = this;
 
-  $ctrl.$onInit = function() {
-    $ctrl.isRegister = ($ctrl.button === 'Sign Up & Create Account') ? true : false;
-  }
+    $ctrl.$onInit = function() {
+        $ctrl.isRegister = ($ctrl.button === 'Sign Up & Create Account') ?
+            true : false;
+        $ctrl.isSubmitted = false;
+    }
+
+    $ctrl.submit = function() {
+        this.onSubmit({
+            $event: {
+                user: $ctrl.user
+            }
+        });
+        $ctrl.isSubmitted = true;
+        console.log($ctrl.user);
+        // $ctrl.onSubmit = $ctrl.user;
+    }
 }
 
 angular
--- src/app/components/auth/auth-register/auth-register.controller.js
+++ src/app/components/auth/auth-register/auth-register.controller.js
@@ -1,20 +1,22 @@
-function RegisterController() {
-  const $ctrl = this;
+function RegisterController(AuthService) {
+    const $ctrl = this;
 
-  $ctrl.$onInit = function() {
-    $ctrl.error = null;
-    $ctrl.user = {
-      name: '',
-      email: '',
-      password: ''
+    $ctrl.$onInit = function() {
+        $ctrl.error = null;
+        $ctrl.user = {
+            username: '',
+            email: '',
+            password: ''
+        }
     }
-  }
 
-  $ctrl.createUser = function(event) {
-    AuthService.register(event.user);
-  }
+    $ctrl.createUser = function(event) {
+        console.log(event);
+        AuthService.register(event);
+    }
 }
 
+RegisterController.$inject = ['AuthService'];
 angular
     .module('components.auth')
     .controller('RegisterController', RegisterController);
