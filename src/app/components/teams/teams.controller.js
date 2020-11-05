d4f374f6ad73980e90d173bcc676c71d6cdb37f1
ckuse <officialconnorkuse@gmail.com>
2020-11-05 09:12:14 -0500

    starter teams stuff (temp)

--- src/app/components/teams/teams.controller.js
+++ src/app/components/teams/teams.controller.js
@@ -1,4 +1,20 @@
-function TeamsController() {}
+function TeamsController() {
+    const $ctrl = this;
+    $ctrl.$onInit = function() {
+        $ctrl.room = {
+            name: "",
+            type: "",
+            teams: {}
+        };
+        $ctrl.roomLoaded = false;
+        TeamModel.getByRoom($ctrl.roomInfo)
+            .then(function(result) {
+                $ctrl.teams = result;
+                $ctrl.teamsLoaded = true;
+            });
+        $ctrl.loaded = true;
+    }
+}
 
 angular.module('components.teams')
     .controller('TeamsController', TeamsController);
